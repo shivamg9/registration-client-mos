7b9918afeb7716e69931c970043bc95f6f564a91
Shivam Rajesh Gupta <56411166+shivamg9@users.noreply.github.com>
2026-02-24 12:50:31 +0530

    Update GenericController.java

--- registration/registration-client/src/main/java/io/mosip/registration/controller/GenericController.java
+++ registration/registration-client/src/main/java/io/mosip/registration/controller/GenericController.java
@@ -2353,9 +2353,7 @@ public class GenericController extends BaseController {
 					if (obj instanceof SimpleDto) {
 						SimpleDto simple = (SimpleDto) obj;
 
-						if ("bur".equals(simple.getLanguage())
-								&& simple.getValue() != null
-								&& simple.getValue().matches(".*[\\u1000-\\u109F].*")) {
+						if ((("bur".equals(simple.getLanguage()))||"fullName".equalsIgnoreCase(key)) && simple.getValue() != null && simple.getValue().matches(".*[\\u1000-\\u109F].*")) {
 
 							simple.setValue(simple.getValue() + "\u200C");
 						}
@@ -2376,6 +2374,7 @@ public class GenericController extends BaseController {
 
 
 }
+
 
 
 
